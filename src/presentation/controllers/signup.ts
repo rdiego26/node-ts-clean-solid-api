1dceb215b4bb3a5d0aeabbb7cbdbaf53e801c61d
ramos <diego.ramos@pipefy.com>
2023-02-05 14:12:12 +0000

    feature: ensure SignUpController returns 400 if no password or password confirmation is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,15 +1,15 @@
 import { type HttpRequest, type HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
-import {badRequest } from "../helpers/http-helper";
+import { badRequest } from '../helpers/http-helper'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body?.name) {
-      return badRequest(new MissingParamError('name'))
-    }
+    const requiredFields: string[] = ['name', 'email', 'password', 'passwordConfirmation']
 
-    if (!httpRequest.body?.email) {
-        return badRequest(new MissingParamError('email'))
+    for (const field of requiredFields) {
+      if (!httpRequest.body?.[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
   }
 }
